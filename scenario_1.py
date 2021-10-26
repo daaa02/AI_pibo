ff5ab44df650fb6a2d5b012ce8f1157a1d9a5595
dah02 <92973975+dah02@users.noreply.github.com>
2021-10-26 21:10:45 +0900

    Update scenario_1.py

--- scenario_1.py
+++ scenario_1.py
@@ -40,21 +40,22 @@ def play_soccer():
     while True:
         time.sleep(2)
         tts('풍선과 천을 준비할 수 있어?')
-        behavior_list.do_waiting()
-        while True:
-            user_input = input("input: ")
-            user_in = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
+        break
 
-            while user_in != -1:
-                if user_in == 'YES':
-                    break
-                elif user_in == 'NO':
-                    tts('이번 놀이는 천이 없어도 할 수 있어!')
-                    break
-                else:
-                    tts('말 다시')
-                    continue
-            break
+    behavior_list.do_waiting()
+    while True:
+        user_input = input("input: ")
+        user_in = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
+
+        while user_in != -1:
+            if user_in == 'YES':
+                break
+            elif user_in == 'NO':
+                tts('이번 놀이는 천이 없어도 할 수 있어!')
+                break
+            else:
+                tts('말 다시')
+                continue
         break
 
     behavior_list.do_waiting()
@@ -76,7 +77,7 @@ def play_soccer():
     while True:
         tts('너가 좋아하는 색깔의 풍선을 골라봐.')
         user_input = input("input: ")
-        user_in = nlp.nlp_done(user_input=user_input, dic=dic)
+        # user_in = nlp.nlp_done(user_input=user_input, dic=dic)
         break
 
     # 3) 놀이 진행
@@ -128,17 +129,16 @@ def play_soccer():
         tts('축구 선수처럼 발 혹은 무릎 높이로 차보자. 준비 됐어?')
         user_input = input("input: ")
         user_in = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
-
-        while user_in != -1:
-            if user_in == 'YES':
-                break
-            elif user_in == 'NO':
-                tts('??')
-                break
-            else:
-                tts('말 다시')
-                continue
         break
+    while user_in != -1:
+        if user_in == 'YES':
+            break
+        elif user_in == 'NO':
+            tts('??')
+            break
+        else:
+            tts('말 다시')
+            continue
 
     behavior_list.do_compliment()
     while True:
@@ -172,25 +172,26 @@ def play_soccer():
 
         user_input = input("input: ")
         user_in = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
-
-        while user_in != -1:
-            if user_in == 'YES':
-                behavior_list.do_sad()
-                while True:
-                    tts('그랬구나, 다음에 그런 일이 또 생기면 주변에 도움을 요청해봐')
-                    time.sleep(5)
-                    break
-                break
-            elif user_in == 'NO':
-                behavior_list.do_joy()
-                while True:
-                    tts('그랬구나!')
-                    time.sleep(5)
-                    break
-                break
-            else:
-                break
         break
+
+    while user_in != -1:
+        if user_in == 'YES':
+            behavior_list.do_sad()
+            while True:
+                tts('그랬구나, 다음에 그런 일이 또 생기면 주변에 도움을 요청해봐')
+                time.sleep(5)
+                break
+            break
+
+        elif user_in == 'NO':
+            behavior_list.do_joy()
+            while True:
+                tts('그랬구나!')
+                time.sleep(5)
+                break
+            break
+        else:
+            break
 
 
     # 6) 놀이 기록
