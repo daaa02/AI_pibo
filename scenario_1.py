b57fd46081ac63fbed3a58bc1ed327f713854a9a
dah02 <92973975+dah02@users.noreply.github.com>
2021-10-26 18:34:44 +0900

    Update scenario_1.py

--- scenario_1.py
+++ scenario_1.py
@@ -36,11 +36,11 @@ def tts(speech_text):
 ###
 def play_soccer():
     # 1) 준비물 설명
-    behavior.do_explain()
+    behavior_list.do_explain()
     while True:
         time.sleep(2)
         tts('풍선과 천을 준비할 수 있어?')
-        behavior.do_waiting()
+        behavior_list.do_waiting()
         while True:
             user_input = input("input: ")
             user_in = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -57,7 +57,7 @@ def play_soccer():
             break
         break
 
-    behavior.do_waiting()
+    behavior_list.do_waiting()
     while True:
         tts('준비가 되면 준비 완료 라고 말해줘')
         user_input = input("input: ")
@@ -72,7 +72,7 @@ def play_soccer():
 
 
     # 2) 놀이 설명
-    behavior.do_suggestion()
+    behavior_list.do_suggestion()
     while True:
         tts('너가 좋아하는 색깔의 풍선을 골라봐.')
         user_input = input("input: ")
@@ -80,7 +80,7 @@ def play_soccer():
         break
 
     # 3) 놀이 진행
-    behavior.do_suggestion()
+    behavior_list.do_suggestion()
     while True:
         tts('자 이제 풍선을 불어보자. 할 수 있지?')
         user_input = input("input: ")
@@ -90,7 +90,7 @@ def play_soccer():
             if user_in == 'YES':
                 break
             elif user_in == 'NO':
-                behavior.do_explain()
+                behavior_list.do_explain()
                 while True:
                     tts('엄마에게 도움을 요청해')
                     time.sleep(3)
@@ -101,29 +101,29 @@ def play_soccer():
                 continue
         break
 
-    behavior.do_suggestion()
+    behavior_list.do_suggestion()
     while True:
         tts('풍선을 묶지 말고 멀리 날려보자!')
-        behavior.do_photo()
+        behavior_list.do_photo()
         while True:
             time.sleep(5)
             print('여기 opencv capture 들어갈 곳. 아마도?')
             break
         break
 
-    behavior.do_joy()
+    behavior_list.do_joy()
     while True:
         time.sleep(2)
         tts('정말 재밌어보여!')
         break
 
-    behavior.do_suggestion()
+    behavior_list.do_suggestion()
     while True:
         tts('다음엔 풍선 축구를 해볼까?')
         break
 
     # 4) 놀이 완료
-    behavior.do_question()
+    behavior_list.do_question()
     while True:
         tts('축구 선수처럼 발 혹은 무릎 높이로 차보자. 준비 됐어?')
         user_input = input("input: ")
@@ -140,17 +140,17 @@ def play_soccer():
                 continue
         break
 
-    behavior.do_compliment()
+    behavior_list.do_compliment()
     while True:
         tts('잘한다! 이번엔 머리로 헤딩슛을 해보자!')
-        behavior.do_cheer()
+        behavior_list.do_cheer()
         while True:
             time.sleep(5)
             break
         break
 
     # 5) 마무리 대화
-    behavior.do_question()
+    behavior_list.do_question()
     while True:
         tts('풍선으로 하는 축구 놀이 재미있었어?')
 
@@ -166,7 +166,7 @@ def play_soccer():
                 break
         break
 
-    behavior.do_tired()
+    behavior_list.do_tired()
     while True:
         tts('파이보는 달리느라 힘들었어. 너는 오늘 힘든 일 있었어?')
 
@@ -175,14 +175,14 @@ def play_soccer():
 
         while user_in != -1:
             if user_in == 'YES':
-                behavior.do_sad()
+                behavior_list.do_sad()
                 while True:
                     tts('그랬구나, 다음에 그런 일이 또 생기면 주변에 도움을 요청해봐')
                     time.sleep(5)
                     break
                 break
             elif user_in == 'NO':
-                behavior.do_joy()
+                behavior_list.do_joy()
                 while True:
                     tts('그랬구나!')
                     time.sleep(5)
@@ -194,11 +194,11 @@ def play_soccer():
 
 
     # 6) 놀이 기록
-    behavior.do_stamp()
+    behavior_list.do_stamp()
     while True:
         tts('오늘 놀이 스탬프를 찍을게')
         break
-    behavior.do_photo()
+    behavior_list.do_photo()
     while True:
         tts('사진을 찍어 줄게. 브이~ ^-^v')
         time.sleep(5)
@@ -206,7 +206,7 @@ def play_soccer():
         break
 
     # 7) 다음 놀이 제안
-    behavior.do_question()
+    behavior_list.do_question()
     while True:
         tts('다음 놀이 할까?')
 
@@ -226,4 +226,3 @@ def play_soccer():
         break
 
     print("시나리오 30: 풍선 축구 놀이 끄읏")
-
