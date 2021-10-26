bb3c333ef3a2ab6face5902c7ae30e37210fd900
dah02 <92973975+dah02@users.noreply.github.com>
2021-10-26 14:41:46 +0900

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -15,31 +15,25 @@ class nlp:
         self.n = 0
 
 
-    def nlp_yes_or_no(self, input, dic):
+    def nlp_yes_or_no(self, user_input, dic):
         user_input = ''
         for i in range(len(dic.yes_or_no['Yes'])):
-            if dic.yes_or_no['Yes'][i] in input:
+            if dic.yes_or_no['Yes'][i] in user_input:
                 user_input = 'YES'  # 'Yes'
         for j in range(len(dic.yes_or_no['No'])):
-            if dic.yes_or_no['No'][j] in input:
+            if dic.yes_or_no['No'][j] in user_input:
                 user_input = 'NO'  # No
-        for k in range(len(dic.yes_or_no['SOSO'])):
-            if dic.yes_or_no['SOSO'][k] in input:
-                user_input = 'SOSO'  # No
+        for k in range(len(dic.yes_or_no['SoSo'])):
+            if dic.yes_or_no['SoSo'][k] in user_input:
+                user_input = 'SOSO'  # SoSo
         return user_input
 
 
-    def nlp_done(self, input, dic):
+    def nlp_done(self, user_input, dic):
         user_input = ''
         for i in range(len(dic.done['Done'])):
-            if dic.done['Done'][i] in input:
-                user_input = 'YES'  # 'Yes'
-        for j in range(len(dic.done['No'])):
-            if dic.done['No'][j] in input:
-                user_input = 'NO'  # No
-        for k in range(len(dic.done['SOSO'])):
-            if dic.done['SOSO'][k] in input:
-                user_input = 'SOSO'  # No
+            if dic.done['Done'][i] in user_input:
+                user_input = 'DONE'  # 'Done'
         return user_input
 
 
@@ -49,7 +43,7 @@ class Dictionary:
             {
                 'Yes': ['네', '예', '응', '어', '있어', '좋아', '그래'],
                 'No': ['아니', '안', '별로', '글쎄', '싫어', '싫', '못하', '못해', '없어', '없네', '없는', '몰라', '모르', '몰라' '그만']
-                'SOSO': ['또', '같은', '한 번 더', '계속']
+                'SoSo': ['또', '같은', '한 번 더', '계속']
             }
         self.done = \
             {
