acd114946c3e25684239820a1fbff75792dcbefc
dah02 <92973975+dah02@users.noreply.github.com>
2021-10-27 23:27:29 +0900

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -9,21 +9,20 @@ from openpibo.audio import Audio
 
 import requests
 import json
+import config
 
 import argparse
 
 audio = Audio()
 
+rest_api_key = 'f8f8c3f66bb3310016fdeccffba152e8'
 
-class mySpeech(Speech):
-  
-    def __init__(self):
-        self.kakao_account = config.get('KAKAO_ACCOUNT')
 
-        
-    def record(self, filename="stream.wav", timeout=5):
+class mySpeech:
 
-        if self.kakao_account in [None, '']:
+    def record(self, filename="stream.wav", timeout=10):
+
+        if srest_api_key in [None, '']:
             raise Exception('Kakao account invalid')
 
         if os.path.isfile(filename):
@@ -32,23 +31,29 @@ class mySpeech(Speech):
         cmd = f"arecord -D hw:2,0 -c1 -r44100 -f S16_LE -d {timeout} -t wav -q -vv -V streo stream.raw;sox stream.raw -c 1 -b 16 {filename};rm stream.raw"
         os.system(cmd)
 
-
     def kakao_stt(self, filename):
-        url = 'https://kakaoi-newtone-openapi.kakao.com/v1/recognize'
+        url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
         headers = {
-            'Content-Type': 'application/octet-stliream',
-            'Authorization': 'KakaoAK ' + self.kakao_account
+            "Content-Type": "application/octet-stream",
+            "X-DSS-Service": "DICTATION",
+            "Authorization": "KakaoAK " + rest_api_key,
         }
 
-        with open(filename, 'rb') as f:
-            data = f.read()
+        with open(filename, 'rb') as fp:
+            data = fp.read()
         res = requests.post(url, headers=headers, data=data)
-        try:
-            result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}') + 1]
-        except Exception as ex:
-            result_json_string = res.text[res.text.index('{"type":"errorCalled"'):res.text.rindex('}') + 1]
+
+        result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}') + 1]
         result = json.loads(result_json_string)
+        print(result['value'])
         return result['value']
+
+        # try:
+        #     result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}') + 1]
+        # except Exception as ex:
+        #     result_json_string = res.text[res.text.index('{"type":"errorCalled"'):res.text.rindex('}') + 1]
+        # result = json.loads(result_json_string)
+        # return result['value']
 
 
     def my_stt(self):
@@ -56,4 +61,3 @@ class mySpeech(Speech):
         self.record(filename)
         result = self.kakao_stt(filename)
         return result
-
