245b074a476995d39e868e7fe6821afcd5868680
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-02 16:22:45 +0900

    Update behavior_list.py
    eye 쓰레드를 비쓰레드화

--- behavior_list.py
+++ behavior_list.py
@@ -14,216 +14,162 @@ audio = Audio()
 
 
 def do_question():
-    # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)    # 'local': pibo speaker, 'hdmi': hdmi monitor
-    # e = Thread(target=eye_list.e_question(), args=())
+    # audio.play('/home/pi/openpibo/question.mp3', out='local', volume=-2000, background=False)
+    eye_list.e_question()
     m = Thread(target=motion_list.m_question(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_suggestion():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_suggestion(), args=())
+    eye_list.e_suggestion()
     m = Thread(target=motion_list.m_suggestion(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_explain():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_explain(), args=())
+    eye_list.e_explain()
     m = Thread(target=motion_list.m_explain(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_photo():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_photo(), args=())
+    eye_list.e_photo()
     m = Thread(target=motion_list.m_photo(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_stamp():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_stamp(), args=())
+    eye_list.e_stamp()
     m = Thread(target=motion_list.m_stamp(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_waiting():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_waiting(), args=())
+    eye_list.e_waiting()
     m = Thread(target=motion_list.m_waiting(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_cheer():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_cheer(), args=())
+    eye_list.e_cheer()
     m = Thread(target=motion_list.m_cheer(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_compliment():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_compliment(), args=())
+    eye_list.e_compliment()
     m = Thread(target=motion_list.m_compliment(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_concil():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_concil(), args=())
+    eye_list.e_concil()
     m = Thread(target=motion_list.m_concil(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_search():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_search(), args=())
+    eye_list.e_search()
     m = Thread(target=motion_list.m_search(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_sleep():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_sleep(), args=())
+    eye_list.e_sleep(),
     m = Thread(target=motion_list.m_sleep(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_wakeup():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_wakeup(), args=())
+    eye_list.e_wakeup()
     m = Thread(target=motion_list.m_wakeup(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_agree():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_agree(), args=())
+    eye_list.e_agree()
     m = Thread(target=motion_list.m_agree(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_deny():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_deny(), args=())
+    eye_list.e_deny()
     m = Thread(target=motion_list.m_deny(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_joy():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_joy(), args=())
+    eye_list.e_joy()
     m = Thread(target=motion_list.m_joy(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_angry():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_angry(), args=())
+    eye_list.e_angry()
     m = Thread(target=motion_list.m_angry(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_sad():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_sad(), args=())
+    eye_list.e_sad()
     m = Thread(target=motion_list.m_sad(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
 
 
 def do_tired():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
-    # e = Thread(target=eye_list.e_tired(), args=())
+    eye_list.e_tired()
     m = Thread(target=motion_list.m_sad(), args=())
 
-    # e.daemon = True
     m.daemon = True
-
-    # e.start()
     m.start()
