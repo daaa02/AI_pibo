a67498b8454ebbebad503a8ac05660c1f1eccdfc
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-03 18:37:47 +0900

    Update m_test.py

--- m_test.py
+++ m_test.py
@@ -17,37 +17,69 @@ def motion_test():
     
     print('explain')
     motion_list.m_explain()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('photo')
     motion_list.m_photo()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('stamp')
     motion_list.m_stamp()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('waiting')
     motion_list.m_waiting()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('cheeeeer')
     motion_list.m_cheer()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('compli')
     motion_list.m_compliment()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('concil')
     motion_list.m_concil()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('search')
     motion_list.m_search()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('sleep')
     motion_list.m_sleep()
     time.sleep(2)
+    
+    print('wakeup')
     motion_list.m_wakeup()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('agree')
     motion_list.m_agree()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('deny')
     motion_list.m_deny()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('joy')
     motion_list.m_joy()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('angry')
     motion_list.m_angry()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('sad')
     motion_list.m_sad()
-    time.sleep(2)
+    time.sleep(5)
+    
+    print('tired')
     motion_list.m_tired()
     time.sleep(2)
+    
+    print('끝')
     
 if __name__ == '__main__':
     motion_test()
