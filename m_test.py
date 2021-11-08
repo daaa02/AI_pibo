95cb146ad7428540de90d914af3ba351d43385d9
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-08 10:40:01 +0900

    Update m_test.py
    모션 마다 init 추가

--- m_test.py
+++ m_test.py
@@ -1,87 +1,165 @@
 import time
 import motion_list
 
+
 def motion_test():
     time.sleep(2)
     
     print('init')
     motion_list.m_init()
-    time.sleep(5)
-    
+    time.sleep(2)
+
     print('question')
     motion_list.m_question()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('suggestion')
     motion_list.m_suggestion()
     time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('explain')
     motion_list.m_explain()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('photo')
     motion_list.m_photo()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('stamp')
     motion_list.m_stamp()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('waiting')
     motion_list.m_waiting()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('cheeeeer')
     motion_list.m_cheer()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('compli')
     motion_list.m_compliment()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('concil')
     motion_list.m_concil()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('search')
     motion_list.m_search()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('sleep')
     motion_list.m_sleep()
     time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('wakeup')
     motion_list.m_wakeup()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('agree')
     motion_list.m_agree()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('deny')
     motion_list.m_deny()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('joy')
     motion_list.m_joy()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('angry')
     motion_list.m_angry()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('sad')
     motion_list.m_sad()
-    time.sleep(5)
+    time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('tired')
     motion_list.m_tired()
     time.sleep(2)
     
+    print('init')
+    motion_list.m_init()
+    time.sleep(2)
+
     print('끝')
-    
+
+
 if __name__ == '__main__':
     motion_test()
+    time.sleep(2)
+    
+    print('init')
+    motion_list.m_init()
     time.sleep(2)
