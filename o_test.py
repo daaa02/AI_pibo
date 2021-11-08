f1ee10abf631624333b0258235b1f5dfdffa2b4b
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-08 14:09:46 +0900

    Update o_test.py
    oled_test_test.py 를 test 하는 o_test.py

--- o_test.py
+++ o_test.py
@@ -1,84 +1,84 @@
 import time
-import oled_list
+import oled_test_test
 
 
 def oled_test():
     time.sleep(2)
 
     print('init')
-    oled_list.o_init()
-    time.sleep(5)
+    oled_test_test.o_init()
+    time.sleep(2)
 
     print('question')
-    oled_list.o_question()
-    time.sleep(5)
+    oled_test_test.o_question()
+    time.sleep(2)
 
     print('suggestion')
-    oled_list.o_suggestion()
+    oled_test_test.o_suggestion()
     time.sleep(2)
 
     print('explain')
-    oled_list.o_explain()
-    time.sleep(5)
+    oled_test_test.o_explain()
+    time.sleep(2)
 
     print('photo')
-    oled_list.o_photo()
-    time.sleep(5)
+    oled_test_test.o_photo()
+    time.sleep(2)
 
     print('stamp')
-    oled_list.o_stamp()
-    time.sleep(5)
+    oled_test_test.o_stamp()
+    time.sleep(2)
 
     print('waiting')
-    oled_list.o_waiting()
-    time.sleep(5)
+    oled_test_test.o_waiting()
+    time.sleep(2)
 
     print('cheeeeer')
-    oled_list.o_cheer()
-    time.sleep(5)
+    oled_test_test.o_cheer()
+    time.sleep(2)
 
     print('compli')
-    oled_list.o_compliment()
-    time.sleep(5)
+    oled_test_test.o_compliment()
+    time.sleep(2)
 
     print('concil')
-    oled_list.o_concil()
-    time.sleep(5)
+    oled_test_test.o_concil()
+    time.sleep(2)
 
     print('search')
-    oled_list.o_search()
-    time.sleep(5)
+    oled_test_test.o_search()
+    time.sleep(2)
 
     print('sleep')
-    oled_list.o_sleep()
+    oled_test_test.o_sleep()
     time.sleep(2)
 
     print('wakeup')
-    oled_list.o_wakeup()
-    time.sleep(5)
+    oled_test_test.o_wakeup()
+    time.sleep(2)
 
     print('agree')
-    oled_list.o_agree()
-    time.sleep(5)
+    oled_test_test.o_agree()
+    time.sleep(2)
 
     print('deny')
-    oled_list.o_deny()
-    time.sleep(5)
+    oled_test_test.o_deny()
+    time.sleep(2)
 
     print('joy')
-    oled_list.o_joy()
-    time.sleep(5)
+    oled_test_test.o_joy()
+    time.sleep(2)
 
     print('angry')
-    oled_list.o_angry()
-    time.sleep(5)
+    oled_test_test.o_angry()
+    time.sleep(2)
 
     print('sad')
-    oled_list.o_sad()
-    time.sleep(5)
+    oled_test_test.o_sad()
+    time.sleep(2)
 
     print('tired')
-    oled_list.o_tired()
+    oled_test_test.o_tired()
     time.sleep(2)
 
     print('끝')
