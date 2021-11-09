d5c65dd1cb108a029dfed2c8da28ff354f7fd3f5
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-09 11:13:13 +0900

    Update behavior_list.py

--- behavior_list.py
+++ behavior_list.py
@@ -19,7 +19,7 @@ audio = Audio()
 def do_question():
     # audio.play('/home/pi/openpibo/question.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_question(), args=())
-    o = Thread(target=oled_list.o_question, args=())
+    o = Thread(target=oled_list.o_question(), args=())
     m = Thread(target=motion_list.m_question(), args=())
 
     # e.daemon = True
@@ -34,7 +34,7 @@ def do_question():
 def do_suggestion():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_suggestion(), args=())
-    o = Thread(target=oled_list.o_suggestion, args=())
+    o = Thread(target=oled_list.o_suggestion(), args=())
     m = Thread(target=motion_list.m_suggestion(), args=())
 
     # e.daemon = True
@@ -49,7 +49,7 @@ def do_suggestion():
 def do_explain():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_explain(), args=())
-    o = Thread(target=oled_list.o_explain, args=())
+    o = Thread(target=oled_list.o_explain(), args=())
     m = Thread(target=motion_list.m_explain(), args=())
 
     # e.daemon = True
@@ -64,7 +64,7 @@ def do_explain():
 def do_photo():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_photo(), args=())
-    o = Thread(target=oled_list.o_photo, args=())
+    o = Thread(target=oled_list.o_photo(), args=())
     m = Thread(target=motion_list.m_photo(), args=())
 
     # e.daemon = True
@@ -79,7 +79,7 @@ def do_photo():
 def do_stamp():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_stamp(), args=())
-    o = Thread(target=oled_list.o_stamp, args=())
+    o = Thread(target=oled_list.o_stamp(), args=())
     m = Thread(target=motion_list.m_stamp(), args=())
 
     # e.daemon = True
@@ -94,7 +94,7 @@ def do_stamp():
 def do_waiting():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_waiting(), args=())
-    o = Thread(target=oled_list.o_waiting, args=())
+    o = Thread(target=oled_list.o_waiting(), args=())
     m = Thread(target=motion_list.m_waiting(), args=())
 
     # e.daemon = True
@@ -109,7 +109,7 @@ def do_waiting():
 def do_cheer():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_cheer(), args=())
-    o = Thread(target=oled_list.o_cheer, args=())
+    o = Thread(target=oled_list.o_cheer(), args=())
     m = Thread(target=motion_list.m_cheer(), args=())
 
     # e.daemon = True
@@ -124,7 +124,7 @@ def do_cheer():
 def do_compliment():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_compliment(), args=())
-    o = Thread(target=oled_list.o_compliment, args=())
+    o = Thread(target=oled_list.o_compliment(), args=())
     m = Thread(target=motion_list.m_compliment(), args=())
 
     # e.daemon = True
@@ -139,7 +139,7 @@ def do_compliment():
 def do_concil():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_concil(), args=())
-    o = Thread(target=oled_list.o_concil, args=())
+    o = Thread(target=oled_list.o_concil(), args=())
     m = Thread(target=motion_list.m_concil(), args=())
 
     # e.daemon = True
@@ -154,7 +154,7 @@ def do_concil():
 def do_search():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_search(), args=())
-    o = Thread(target=oled_list.o_search, args=())
+    o = Thread(target=oled_list.o_search(), args=())
     m = Thread(target=motion_list.m_search(), args=())
 
     # e.daemon = True
@@ -169,7 +169,7 @@ def do_search():
 def do_sleep():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_sleep(), args=())
-    o = Thread(target=oled_list.o_sleep, args=())
+    o = Thread(target=oled_list.o_sleep(), args=())
     m = Thread(target=motion_list.m_sleep(), args=())
 
     # e.daemon = True
@@ -184,7 +184,7 @@ def do_sleep():
 def do_wakeup():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_wakeup(), args=())
-    o = Thread(target=oled_list.o_wakeup, args=())
+    o = Thread(target=oled_list.o_wakeup(), args=())
     m = Thread(target=motion_list.m_wakeup(), args=())
 
     # e.daemon = True
@@ -199,7 +199,7 @@ def do_wakeup():
 def do_agree():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_agree(), args=())
-    o = Thread(target=oled_list.o_agree, args=())
+    o = Thread(target=oled_list.o_agree(), args=())
     m = Thread(target=motion_list.m_agree(), args=())
 
     # e.daemon = True
@@ -214,7 +214,7 @@ def do_agree():
 def do_deny():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_deny(), args=())
-    o = Thread(target=oled_list.o_deny, args=())
+    o = Thread(target=oled_list.o_deny(), args=())
     m = Thread(target=motion_list.m_deny(), args=())
 
     # e.daemon = True
@@ -229,7 +229,7 @@ def do_deny():
 def do_joy():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_joy(), args=())
-    o = Thread(target=oled_list.o_joy, args=())
+    o = Thread(target=oled_list.o_joy(), args=())
     m = Thread(target=motion_list.m_joy(), args=())
 
     # e.daemon = True
@@ -244,7 +244,7 @@ def do_joy():
 def do_angry():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_angry(), args=())
-    o = Thread(target=oled_list.o_angry, args=())
+    o = Thread(target=oled_list.o_angry(), args=())
     m = Thread(target=motion_list.m_angry(), args=())
 
     # e.daemon = True
@@ -259,7 +259,7 @@ def do_angry():
 def do_sad():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_sad(), args=())
-    o = Thread(target=oled_list.o_sad, args=())
+    o = Thread(target=oled_list.o_sad(), args=())
     m = Thread(target=motion_list.m_sad(), args=())
 
     # e.daemon = True
@@ -274,7 +274,7 @@ def do_sad():
 def do_tired():
     # audio.play('/home/pi/openpibo/~.mp3', out='local', volume=-2000, background=False)
     # e = Thread(target=eye_list.e_tired(), args=())
-    o = Thread(target=oled_list.o_tired, args=())
+    o = Thread(target=oled_list.o_tired(), args=())
     m = Thread(target=motion_list.m_sad(), args=())
 
     # e.daemon = True
