7438276eb8c4e4feb7ee2658d9fb0727c0c7f985
dah02 <92973975+dah02@users.noreply.github.com>
2021-11-10 19:18:59 +0900

    Update scenario_1.py

--- scenario_1.py
+++ scenario_1.py
@@ -1,22 +1,17 @@
 # 1. 시나리오 구현: 대근육/소근육 놀이
 
 import os
-import sys
 import time
 import json
 import requests
-from threading import Thread
-from konlpy.tag import Mecab
 
 import openpibo
-import openpibo_models
 from openpibo.speech import Speech
 from openpibo.audio import Audio
 
 import motion_list
 import eye_list
 import behavior_list
-
 from NLP import nlp, Dictionary
 
 nlp = nlp()
@@ -24,14 +19,6 @@ dic = Dictionary()
 
 speech = Speech()
 audio = Audio()
-
-
-# def record(self, filename="stream.wav", timeout=10):
-#     if os.path.isfile(filename):
-#         os.remove(filename)
-
-#     cmd = f"arecord default -c1 -r16000 -f S16_LE -d {timeout} -t wav -q -vv -V streo stream.raw;sox stream.raw -c 1 -b 16 {filename};rm stream.raw"
-#     os.system(cmd)
 
 
 def stt():
@@ -45,9 +32,9 @@ def stt():
     }
 
     with open('stream.wav', 'rb') as fp:
-        audio = fp.read()
+        stream = fp.read()
 
-    res = requests.post(kakao_speech_url, headers=headers, data=audio)
+    res = requests.post(kakao_speech_url, headers=headers, data=stream)
     # print(res.text)
 
     result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}') + 1]
@@ -58,14 +45,11 @@ def stt():
 
 
 def tts(speech_text):
-    speech = Speech()
-    audio = Audio()
-    
     file = openpibo.config['DATA_PATH'] + "/tts.wav"
     speech.tts(f"<speak>\
                 <voice name='WOMAN_READ_CALM'><prosody rate='slow'>{speech_text}<break time='500ms'/></prosody></voice>\
                 </speak>", file)
-    audio.play(file, 'local', '-1500', False)
+    audio.play(file, 'local', '-1000', False)
     print("\n")
     print(speech_text)
 
@@ -73,15 +57,15 @@ def tts(speech_text):
 ###
 def play_balloon_soccer():
     # 1) 준비물 설명
-    behavior_list.do_explain()
+    behavior_list.do_question()
     while True:
-        print("\n")
+        time.sleep(1)
         tts('풍선과 천을 준비할 수 있어?')
         break
 
     behavior_list.do_waiting()
     while True:
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -91,10 +75,14 @@ def play_balloon_soccer():
                 print(answer)
             elif answer == 'NO':
                 print(answer)
-                tts('이번 놀이는 천이 없어도 할 수 있어!')
+                behavior_list.do_explain()
+                while True:
+                    time.sleep(1)
+                    tts('이번 놀이는 천이 없어도 할 수 있어!')
+                    break
             else:
                 tts('말 다시')
-                os.system('python record.py')
+                os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
                 user_input = stt()
                 # user_input = input("input: ")
                 answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -105,9 +93,10 @@ def play_balloon_soccer():
 
     behavior_list.do_waiting()
     while True:
+        time.sleep(1)
         tts('풍선 준비가 되면 준비 완료 라고 말해줘')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_done(user_input=user_input, dic=dic)
@@ -121,9 +110,10 @@ def play_balloon_soccer():
     # 2) 놀이 설명
     behavior_list.do_suggestion()
     while True:
+        time.sleep(1)
         tts('너가 좋아하는 색깔의 풍선을 골라봐.')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_done(user_input=user_input, dic=dic)
@@ -133,8 +123,9 @@ def play_balloon_soccer():
     # 3) 놀이 진행
     behavior_list.do_suggestion()
     while True:
+        time.sleep(1)
         tts('자 이제 풍선을 불어보자. 할 수 있지?')
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -152,34 +143,37 @@ def play_balloon_soccer():
 
     behavior_list.do_suggestion()
     while True:
+        time.sleep(1)
         tts('풍선을 묶지 말고 멀리 날려보자!')
         time.sleep(2)
         break
 
     behavior_list.do_photo()
     while True:
-        time.sleep(5)
-        print('여기 행동 촬영 들어갈 곳. 아마도?')
+        time.sleep(1)
+        print('---여기 행동 촬영 들어갈 곳 1---')
         time.sleep(2)
         break
 
     behavior_list.do_joy()
     while True:
-        time.sleep(2)
+        time.sleep(1)
         tts('정말 재밌어보여!')
         break
 
     behavior_list.do_suggestion()
     while True:
+        time.sleep(1)
         tts('다음엔 풍선 축구를 해볼까?')
         break
 
     # 4) 놀이 완료
     behavior_list.do_question()
     while True:
+        time.sleep(1)
         tts('축구 선수처럼 발 혹은 무릎 높이로 차보자. 준비 됐어?')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_done(user_input=user_input, dic=dic)
@@ -197,17 +191,14 @@ def play_balloon_soccer():
         tts('잘한다! 이번엔 머리로 헤딩슛을 해보자!')
         break
 
-    behavior_list.do_cheer()
-    while True:
-        time.sleep(5)
-        break
 
     # 5) 마무리 대화
     behavior_list.do_question()
     while True:
+        time.sleep(1)
         tts('풍선으로 하는 축구 놀이 재미있었어?')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -224,7 +215,7 @@ def play_balloon_soccer():
     while True:
         tts('파이보는 달리느라 힘들었어. 너는 오늘 힘든 일 있었어?')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -233,16 +224,16 @@ def play_balloon_soccer():
             print(answer)
             behavior_list.do_sad()
             while True:
+                time.sleep(2)
                 tts('그랬구나, 다음에 그런 일이 또 생기면 주변에 도움을 요청해봐')
-                time.sleep(5)
                 break
 
         elif answer == 'NO':
             print(answer)
             behavior_list.do_joy()
             while True:
+                time.sleep(2)
                 tts('그랬구나!')
-                time.sleep(5)
                 break
         # else:
         #     break
@@ -251,26 +242,25 @@ def play_balloon_soccer():
     # 6) 놀이 기록
     behavior_list.do_stamp()
     while True:
-        time.sleep(3)
+        time.sleep(1)
         tts('오늘은 튼튼 스탬프를 찍어줄게')
         break
-
+    
+    tts('사진을 찍어 줄게. 브이~ ^-^v')
     behavior_list.do_photo()
     while True:
-        tts('사진을 찍어 줄게. 브이~ ^-^v')
-
-        time.sleep(5)
-        print('여기 행동 촬영 들어갈 곳. 아마도?')
+        time.sleep(1)
+        print('---여기 행동 촬영 들어갈 곳 2---')
         time.sleep(2)
         break
 
-        
     # 7) 다음 놀이 제안
     behavior_list.do_question()
     while True:
+        time.sleep(1)
         tts('다음 놀이 할까?')
 
-        os.system('python record.py')
+        os.system("arecord -t wav -c 1 -D plughw:1,0 -f S16_LE -d 5 -r 16000 stream.wav")
         user_input = stt()
         # user_input = input("input: ")
         answer = nlp.nlp_yes_or_no(user_input=user_input, dic=dic)
@@ -285,7 +275,7 @@ def play_balloon_soccer():
         #     tts('말 다시')
         #     continue
         break
-    
+
     motion_list.m_init()
-    
+
     print("\n\n**시나리오 30: 풍선 축구 놀이 끄읏**\n\n")
