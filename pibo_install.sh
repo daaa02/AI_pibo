23676fe0a825dcffd7a535b271152d10577b4dbe
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-07 15:58:11 +0900

    mkdir openpibo

--- pibo_install.sh
+++ pibo_install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 echo "install openpibo os"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1c7yACdRlR7aM87xkV0wY1ElHurEeKfhW' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1c7yACdRlR7aM87xkV0wY1ElHurEeKfhW" -O EOS_PIBO_211206_V2.7z && rm -rf /tmp/cookies.txt
 
 echo "pip install openpibo"
 sudo pip3 install openpibo-python
+
+cd 
+mkdir openpibo
+cd openpibo
 
 echo "install openpibo github"
 git clone https://github.com/themakerrobot/themakerrobot.git
