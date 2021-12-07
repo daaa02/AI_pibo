6191bd6d30211938338ef42a4ddf1e8f384ac8e4
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-07 18:15:09 +0900

    Update pibo_install.sh

--- pibo_install.sh
+++ pibo_install.sh
@@ -10,7 +10,7 @@ echo "pip install openpibo"
 sudo pip3 install openpibo-python
 
 echo "install openpibo github"
-cd ../
+cd ../openpibo
 git clone https://github.com/themakerrobot/themakerrobot.git
 git clone https://github.com/themakerrobot/openpibo-python.git
 git clone https://github.com/themakerrobot/openpibo-tools.git
